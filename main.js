e91a55334afcfb2250a986899954bf69a3b12236
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-05-24 15:28:47 -0500

    commits reformat

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 "use strict"
 
-function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    html += '<div>' + coffee.id + '</div>';
-    html += '<div>' + coffee.name + '</div>';
-    html += '<div>' + coffee.roast + '</div>';
-    html += '</div>';
-
-    return html;
+function buildCoffee(coffee) {
+    let coffeeDiv = document.createElement('div')
+    coffeeDiv.innerHTML =
+           `<div> ${coffee.id} </div>
+            <div> ${coffee.name} </div>
+            <div>${coffee.roast} </div>`;
+    return coffeeDiv;
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
+    for (var i = coffees.length - 1; i >= 0; i--) {
+        console.log(tbody)
+        tbody.appendChild(buildCoffee(coffees[i]));
     }
-    return html;
 }
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
+    coffees.forEach(function (coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    renderCoffees(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -52,6 +50,6 @@ var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
+renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
